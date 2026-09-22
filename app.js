cc2c91f86f5cb71ffde70fc6212ab97978911ad7
erick24501-source <erick24501@cbtis75.edu.mx>
2026-09-22 07:46:00 -0600

    feat: integracion de endpoints de detalles y creadores

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function mostrarJuegosEnPantalla(juegos) {
 }
 
 // -------------------------------------------------------------
-// METODOS INTEGRANTE 2: DETALLES, CAPTURAS Y CREADORES
+//// Trabajo realizado por Erick: Detalles, capturas y creadores
 // -------------------------------------------------------------
 // Método 3: Detalle de un juego por ID
 async function obtenerDetalleJuego(id) {
