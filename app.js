a640f38b65654ed4239c581a0d55e7e7561be421
yamil24486-blip <yamil24486@cbtis75.edu.mx>
2026-09-22 08:13:22 -0600

    feat: ajuste de SweetModal para generos

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ async function obtenerCreadores() {
 }
 
 // -------------------------------------------------------------
-// Trabajo realizado por Yamil: Géneros, plataformas y editores
+// Trabajo realizado por Yamil: Géneros, plataformas y editores,
 // -------------------------------------------------------------
 // Método 6: Géneros
 async function obtenerGeneros() {
